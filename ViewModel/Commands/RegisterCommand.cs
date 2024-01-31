21647b8b9e61f9e4c652f7afc50e8ac115105f00
Vitalii <seminenkovs@gmail.com>
2024-01-31 02:23:44 +0100

    Created RegisterCommand

---
+++ ViewModel/Commands/RegisterCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Input;
+
+namespace EvernoteCloneWPF.ViewModel.Commands;
+
+public class RegisterCommand : ICommand
+{
+    public event EventHandler? CanExecuteChanged;
+
+    public LoginVM VM { get; set; }
+
+    public RegisterCommand(LoginVM vm)
+    {
+        VM = vm;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        //TODO loging functionality
+    }
+}
