20a6ad37c7d3407dfe767fd54ba2e9718fc53215
Vitalii <seminenkovs@gmail.com>
2024-01-31 02:19:49 +0100

    Created LoginVM

---
+++ ViewModel/LoginVM.cs
@@ -0,0 +1,14 @@
+﻿using EvernoteCloneWPF.Model;
+
+namespace EvernoteCloneWPF.ViewModel;
+
+public class LoginVM
+{
+	private User _user;
+
+	public User User
+	{
+		get { return _user; }
+		set { _user = value; }
+	}
+}
