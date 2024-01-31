695a49c36aa9367936949679df0ddb8d2a992425
Vitalii <seminenkovs@gmail.com>
2024-01-31 02:33:55 +0100

    Created NotesVM

---
+++ ViewModel/NotesVM.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using EvernoteCloneWPF.Model;
+
+namespace EvernoteCloneWPF.ViewModel;
+
+public class NotesVM
+{
+    public ObservableCollection<Notebook> Notebooks { get; set; }
+    public ObservableCollection<Note> Notes { get; set; }
+
+	private Notebook _selectNotebook;
+
+	public Notebook SelectedNotebook
+	{
+		get { return _selectNotebook; }
+        set
+        {
+            _selectNotebook = value;
+            //TODO: get notes
+        }
+	}
+
+
+
+}
