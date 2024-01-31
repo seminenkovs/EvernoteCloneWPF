847f3443344b594106be984799b19a5750e1768a
Vitalii <seminenkovs@gmail.com>
2024-01-31 01:51:43 +0100

    Created model User

--- Model/User.cs
+++ Model/User.cs
@@ -2,5 +2,9 @@
 
 public class User
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
 }
