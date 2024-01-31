3dab64bfc0d35809c0476af74e2e88f89b6bc1db
Vitalii <seminenkovs@gmail.com>
2024-01-31 01:59:11 +0100

    installed sqlite-net-pcl

--- Model/User.cs
+++ Model/User.cs
@@ -1,9 +1,14 @@
-﻿namespace EvernoteCloneWPF.Model;
+﻿using SQLite;
+
+namespace EvernoteCloneWPF.Model;
 
 public class User
 {
+    [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
+    [MaxLength(50)]
     public string Name { get; set; }
+    [MaxLength(50)]
     public string LastName { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
