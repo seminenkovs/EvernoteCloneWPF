b019df8bce4d25cb91d271bf52f65536063af18c
Vitalii <seminenkovs@gmail.com>
2024-01-31 01:54:48 +0100

    Created model Note

--- Model/Note.cs
+++ Model/Note.cs
@@ -2,5 +2,10 @@
 
 public class Note
 {
-    
+    public int Id { get; set; }
+    public int NoteBookId { get; set; }
+    public string Title { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set;}
+    public string FileLocation { get; set; }
 }
