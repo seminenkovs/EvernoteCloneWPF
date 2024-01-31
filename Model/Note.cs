6b67d4f9fadeb139ea2d858c12f5db0bbaf032ad
Vitalii <seminenkovs@gmail.com>
2024-01-31 02:01:03 +0100

    Added attributes to models

--- Model/Note.cs
+++ Model/Note.cs
@@ -1,8 +1,12 @@
-﻿namespace EvernoteCloneWPF.Model;
+﻿using SQLite;
+
+namespace EvernoteCloneWPF.Model;
 
 public class Note
 {
+    [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
+    [Indexed]
     public int NoteBookId { get; set; }
     public string Title { get; set; }
     public DateTime CreatedAt { get; set; }
--- Model/Notebook.cs
+++ Model/Notebook.cs
@@ -1,8 +1,12 @@
-﻿namespace EvernoteCloneWPF.Model;
+﻿using SQLite;
+
+namespace EvernoteCloneWPF.Model;
 
 public class Notebook
 {
+    [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
+    [Indexed]
     public int UserId { get; set; }
     public string Name { get; set; }
 }
