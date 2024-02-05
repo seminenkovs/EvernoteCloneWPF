99146e9847c4bdc86634f75d292c9678a5f04506
Vitalii <seminenkovs@gmail.com>
2024-02-05 18:33:01 +0100

    ToggleButton Bold

--- View/NotesWindow.xaml.cs
+++ View/NotesWindow.xaml.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Speech.Recognition;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using Microsoft.CognitiveServices.Speech;
-using Microsoft.CognitiveServices.Speech.Audio;
+
 
 namespace EvernoteCloneWPF.View
 {
@@ -21,9 +22,35 @@ namespace EvernoteCloneWPF.View
     /// </summary>
     public partial class NotesWindow : Window
     {
+        //private SpeechRecognitionEngine _recognizer;
+
         public NotesWindow()
         {
             InitializeComponent();
+
+            #region System.Speech speech recognizer
+            //var currentCulture = (from r in SpeechRecognitionEngine.InstalledRecognizers()
+            //    where r.Culture.Equals(Thread.CurrentThread.CurrentCulture)
+            //    select r).FirstOrDefault();
+
+            //_recognizer = new SpeechRecognitionEngine(currentCulture);
+
+            //GrammarBuilder builder = new GrammarBuilder();
+            //builder.AppendDictation();
+            //Grammar grammar = new Grammar(builder);
+
+            //_recognizer.LoadGrammar(grammar);
+            //_recognizer.SetInputToDefaultAudioDevice();
+            //_recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
+            #endregion 
+        }
+
+        private void Recognizer_SpeechRecognized(object? sender, SpeechRecognizedEventArgs e)
+        {
+            string recognizedText = e.Result.Text;
+
+            contentRichTextBox.Document.Blocks.Add(new Paragraph(
+                new Run(recognizedText)));
         }
 
         private void MenuItem_OnClick_Exit(object sender, RoutedEventArgs e)
@@ -31,22 +58,41 @@ namespace EvernoteCloneWPF.View
             Application.Current.Shutdown();
         }
 
+        //private bool isRecognizing = false;
         private async void SpeechButton_OnClick(object sender, RoutedEventArgs e)
         {
-            // TODO: get region and key for evernote speech to text
-            string region = "";
-            string key = "";
+            #region Microsoft.CognitiveServices.Speech
 
-            var speechConfig = SpeechConfig.FromSubscription(key, region);
-            using (var audioConfig = AudioConfig.FromDefaultMicrophoneInput())
-            {
-                using (var recognizer = new SpeechRecognizer(speechConfig, audioConfig))
-                {
-                    var result = await recognizer.RecognizeOnceAsync();
-                    contentRichTextBox.Document.Blocks.Add(new Paragraph(
-                        new Run(result.Text)));
-                }
-            }
+            //get region and key for evernote speech to text
+            //string region = "";
+            //string key = "";
+
+            //var speechConfig = SpeechConfig.FromSubscription(key, region);
+            //using (var audioConfig = AudioConfig.FromDefaultMicrophoneInput())
+            //{
+            //    using (var recognizer = new SpeechRecognizer(speechConfig, audioConfig))
+            //    {
+            //        var result = await recognizer.RecognizeOnceAsync();
+            //        contentRichTextBox.Document.Blocks.Add(new Paragraph(
+            //            new Run(result.Text)));
+            //    }
+            //}
+
+            #endregion
+
+            #region System.Speech
+
+            //if (!isRecognizing)
+            //{
+            //    _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            //}
+            //else
+            //{
+            //    _recognizer.RecognizeAsyncStop();
+            //    isRecognizing = false;
+            //}
+            
+            #endregion
         }
 
         private void ContentRichTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
@@ -59,8 +105,27 @@ namespace EvernoteCloneWPF.View
 
         private void BoldButton_OnClick(object sender, RoutedEventArgs e)
         {
-            contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty,
-                FontWeights.Bold);
+            bool isButtonChecked = (sender as ToggleButton).IsChecked ?? false;
+
+            if (isButtonChecked)
+            {
+                contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty,
+                    FontWeights.Bold);
+            }
+            else
+            {
+                contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty,
+                    FontWeights.Normal);
+            }
+            
+        }
+
+        private void ContentRichTextBox_OnSelectionChanged(object sender, RoutedEventArgs e)
+        {
+            var selectedWeight = contentRichTextBox.Selection
+                .GetPropertyValue(FontWeightProperty);
+            boldButton.IsChecked = (selectedWeight != DependencyProperty.UnsetValue) &&
+                                   (selectedWeight.Equals(FontWeights.Bold));
         }
     }
 }
