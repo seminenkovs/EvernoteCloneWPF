86e4654e8061363de908995b6c9b727ecf4ca02e
Vitalii <seminenkovs@gmail.com>
2024-02-06 18:57:01 +0100

    Created UserControl DisplayNotebook

---
+++ View/UserControls/DisplayNotebook.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EvernoteCloneWPF.Model;
+
+namespace EvernoteCloneWPF.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for DisplayNotebook.xaml
+    /// </summary>
+    public partial class DisplayNotebook : UserControl
+    {
+
+        public Notebook Notebook
+        {
+            get { return (Notebook)GetValue(NotebookProperty); }
+            set { SetValue(NotebookProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NotebookProperty =
+            DependencyProperty.Register("Notebook", typeof(Notebook), typeof(DisplayNotebook), new PropertyMetadata(null, SetValues));
+
+        private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            DisplayNotebook notebookUserControl = (DisplayNotebook)d;
+
+            if (notebookUserControl != null)
+            {
+                notebookUserControl.DataContext = notebookUserControl.Notebook;
+            }
+        }
+
+        public DisplayNotebook()
+        {
+            InitializeComponent();
+        }
+    }
+}
